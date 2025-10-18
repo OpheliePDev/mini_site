5702c64ccb7fee21826b12d0a5b2135c2138875c
Ophélie <ophelipereira28@gmail.com>
2025-10-18 22:47:58 +0200

    First commit of my Symfony refresher

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'title' => 'À propos de moi',
+        ]);
+    }
+    #[Route('contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'title' => 'Contact',
+        ]);
+    }
+}
