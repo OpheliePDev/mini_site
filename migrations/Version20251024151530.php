aaf874dcf2e3798eb85201617b0f8bad856d9cde
Ophélie <ophelipereira28@gmail.com>
2025-10-24 18:07:09 +0200

    Minor changes

--- migrations/Version20251024151530.php
+++ migrations/Version20251024151530.php
@@ -20,7 +20,7 @@ final class Version20251024151530 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE task CHANGE description description LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE task CHANGE description description LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
